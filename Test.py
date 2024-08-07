a0b4b1be9c0d550ff89b0aae65f5162c2ecef828
Cesar Silva <cesar.silva@controlt.com.co>
2024-08-06 21:47:58 -0500

    mi primer funcion python

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-print("Hola, Oscar")
+def suma(a, b):
+    """
+    Esta función toma dos números y devuelve su suma.
+    
+    Parámetros:
+    a (int, float): El primer número.
+    b (int, float): El segundo número.
+    
+    Retorna:
+    int, float: La suma de los dos números.
+    """
+    return a + b
